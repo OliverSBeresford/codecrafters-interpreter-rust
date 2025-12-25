97dc60cfacb7c1fe2f6848a15e5eb651266752b2
Oliver Beresford <oggynaut@icloud.com>
2025-12-24 19:13:13 -0500

    Add documentation for ControlFlow enum

--- src/runtime/control_flow.rs
+++ src/runtime/control_flow.rs
@@ -1,6 +1,7 @@
 use crate::runtime::runtime_error::RuntimeError;
 use crate::runtime::value::Value;
 
+/// Enum used to represent control flow changes during interpretation, such as returning a value or encountering a runtime error.
 #[derive(Debug)]
 pub enum ControlFlow {
     Return(Value),
